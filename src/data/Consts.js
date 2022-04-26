b134c7528200d010d62cab4ad263d86dd4854222
Stephen Pearson <spearson@sra.com.au>
2022-04-26 14:03:17 +0930

    move consts to new file for easier re-use

---
+++ src/data/Consts.js
@@ -0,0 +1,21 @@
+//constants
+const SKILLS = {
+	STR:'STR',
+	BRV:'BRV',
+	INT:'INT'
+};
+const MAX_SANITY = 5;
+const MAX_FATIGUE = 3;
+const MAX_ABILITY_POINTS = 4;
+const EVENT_TYPES = {
+	COMBAT:"combat",
+	//TODO: others? ambulance, construction, etc?
+};
+
+export {
+	SKILLS,
+	MAX_SANITY,
+	MAX_FATIGUE,
+	MAX_ABILITY_POINTS,
+	EVENT_TYPES
+};
