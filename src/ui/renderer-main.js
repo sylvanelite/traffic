62d25c1698848e8680d24c62227590050205eb0b
Sylvan <sylvanelite@hotmail.com>
2022-04-28 21:02:39 +0930

    adding stat bar and proper UI rendering.

--- src/ui/renderer-main.js
+++ src/ui/renderer-main.js
@@ -2,9 +2,58 @@
 import { Renderer } from "./renderer.js";
 import { AreaData } from "../data/AreaData.js";
 
+import {
+	SKILLS,
+	MAX_SANITY,
+	MAX_FATIGUE,
+	MAX_ABILITY_POINTS,
+	EVENT_TYPES
+} from "../data/Consts.js";
+
+
 class RenderMain{
 	
 	static #sprites = {
+		
+		ui:{
+			sanity_full:Renderer.getSprite(
+				'ui/ui_symbols.png',
+				0,33,7,7,0,0
+			),
+			sanity_empty:Renderer.getSprite(
+				'ui/ui_symbols.png',
+				0,33,7,7,0,7
+			),
+			fatigue_full:Renderer.getSprite(
+				'ui/ui_symbols.png',
+				0,43,10,10,7,0
+			),
+			fatigue_empty:Renderer.getSprite(
+				'ui/ui_symbols.png',
+				0,43,10,10,17,0
+			),
+			ap_full:Renderer.getSprite(
+				'ui/ui_symbols.png',
+				0,54,15,12,27,0
+			),
+			ap_empty:Renderer.getSprite(
+				'ui/ui_symbols.png',
+				0,54,15,12,42,0
+			),
+			skil_int:Renderer.getSprite(
+				'ui/ui_symbols.png',
+				164,32,29,17,0,14
+			),
+			skil_brv:Renderer.getSprite(
+				'ui/ui_symbols.png',
+				164,32,29,17,31,14
+			),
+			skil_str:Renderer.getSprite(
+				'ui/ui_symbols.png',
+				164,32,29,17,0,32
+			),
+		},
+		
 		bg:Renderer.getSprite(
 			'ui/0_bg.png',
 			0,0,980,540,0,0
@@ -79,6 +128,67 @@ class RenderMain{
 		},
 	};
 	
+	static #drawStats = (ctx,ch,x,y)=>{
+		for(let i=0;i<MAX_SANITY;i+=1){
+			if(i<ch.sanity){
+				//draw sprite sanity
+				const spriteSanity = RenderMain.#sprites.ui.sanity_full;
+				spriteSanity.x=x+119+spriteSanity.width*i;
+				Renderer.drawSprite(spriteSanity,ctx);
+			}else{
+				//draw sprite sanity missing
+				const spriteSanityEmpty = RenderMain.#sprites.ui.sanity_empty;
+				spriteSanityEmpty.x=x+119+spriteSanityEmpty.width*i;
+				Renderer.drawSprite(spriteSanityEmpty,ctx);
+			}
+		}
+		for(let i=0;i<MAX_FATIGUE;i+=1){
+			if(i<ch.fatigue){
+				//draw sprite fatigue
+				const spriteFatigue = RenderMain.#sprites.ui.fatigue_full;
+				spriteFatigue.x=x+105+spriteFatigue.width*i;
+				Renderer.drawSprite(spriteFatigue,ctx);
+			}else{
+				//draw sprite fatigue missing
+				const spriteFatigueEmpty = RenderMain.#sprites.ui.fatigue_empty;
+				spriteFatigueEmpty.x=x+105+spriteFatigueEmpty.width*i;
+				Renderer.drawSprite(spriteFatigueEmpty,ctx);
+			}
+		}
+		for(let i=0;i<MAX_ABILITY_POINTS;i+=1){
+			if(i<ch.ability_points){
+				//draw sprite ap
+				const spriteAP = RenderMain.#sprites.ui.ap_full;
+				spriteAP.x=x+105+spriteAP.width*i;
+				Renderer.drawSprite(spriteAP,ctx);
+			}else{
+				//draw sprite ap missing
+				const spriteAPEmpty = RenderMain.#sprites.ui.ap_empty;
+				spriteAPEmpty.x=x+105+spriteAPEmpty.width*i;
+				Renderer.drawSprite(spriteAPEmpty,ctx);
+			}
+		}
+		//draw stat element
+		switch(ch.skill_type){
+			case SKILLS.STR:
+				RenderMain.#sprites.ui.skil_str.x = 164+x;
+				Renderer.drawSprite(RenderMain.#sprites.ui.skil_str,ctx);
+				break;
+			case SKILLS.INT:
+				RenderMain.#sprites.ui.skil_int.x = 164+x;
+				Renderer.drawSprite(RenderMain.#sprites.ui.skil_int,ctx);
+				break;
+			case SKILLS.BRV:
+				RenderMain.#sprites.ui.skil_brv.x = 164+x;
+				Renderer.drawSprite(RenderMain.#sprites.ui.skil_brv,ctx);
+				break;
+		}
+		//TODO:
+		//draw attack value
+		//draw HP bar
+		
+	}
+	
 	static render(G,ctx,context){//ctx here is canvas, not the G ctx
 		Renderer.drawSprite(RenderMain.#sprites.bg,ctx);
 		Renderer.drawSprite(RenderMain.#sprites.map_towna,ctx);//TODO: read the town from the G data
@@ -89,6 +199,7 @@ class RenderMain{
 			Renderer.drawSprite(spriteStats,ctx);
 			const spritePortrait = RenderMain.#sprites.characters[name].portrait;
 			Renderer.drawSprite(spritePortrait,ctx);
+			RenderMain.#drawStats(ctx,ch,spritePortrait.x,spritePortrait.y);
 		}
 		Renderer.drawSprite(RenderMain.#sprites.header_front,ctx);//TODO: read the town from the G data
 		let x=24;
