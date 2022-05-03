d552a7792d3bf3f316f53711241a3aea2567ab8f
Stephen Pearson <spearson@sra.com.au>
2022-05-03 16:15:17 +0930

    adding scripts and regions placholders.

--- src/data/AreaData.js
+++ src/data/AreaData.js
@@ -6,10 +6,6 @@ const AreaData = {};
 const areaA = {
 	name:'areaA',//TODO: render data (x,y, icon,etc?)
 	towns:[
-	{name:'hospital',display:'54',
-	starA:'first_visit',starB:'script_a_complete',starC:STAR_NONE,
-	x:421,y:144
-	},
 	{name:'quest_18',display:'15',
 	starA:STAR_NONE,starB:STAR_NONE,starC:STAR_NONE,
 	x:119,y:276
@@ -18,50 +14,353 @@ const areaA = {
 	starA:STAR_NONE,starB:STAR_NONE,starC:STAR_NONE,
 	x:277,y:270
 	},
-	{name:'a_16',display:'16',
+	{name:'lesson_A',display:'16',
 	starA:STAR_NONE,starB:STAR_NONE,starC:STAR_NONE,
 	x:277,y:330
-	},
-	{name:'a_18',display:'18',
-	starA:STAR_NONE,starB:STAR_NONE,starC:STAR_NONE,
-	x:409,y:274
-	},
-	{name:'a_19',display:'19',
-	starA:STAR_NONE,starB:STAR_NONE,starC:STAR_NONE,
-	x:554,y:337
-	},
-	
-	
-		{name:'townA',display:'townA',
-		starA:STAR_NONE,//keyword to lock/unlock a star, or none to hide star completely
-		starB:STAR_NONE,
-		starC:STAR_NONE,
-		x:0,//location of the town icon
-		y:0
-		}//TODO: render data 
+	}
 	],
 	neighbours:[{display:'area B',
-		name:'areaB',//TODO: render data (x,y, icon,etc?)
+		name:'areaB',
+	},{display:'area D',
+		name:'areaD',
 	}],
 };
 const areaB = {
-	name:'areaB',//TODO: render data (x,y, icon,etc?)
+	name:'areaB',
 	towns:[
-		{name:'townB',display:'townB',
+		{name:'quest_16',display:'quest_16',
 		starA:STAR_NONE,
 		starB:STAR_NONE,
 		starC:STAR_NONE,
 		x:0,
 		y:0
-		}//TODO: render data (x,y, icon,etc?)
+		},
+		{name:'hospital',display:'hospital',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:100,
+		y:100
+		},
+		{name:'quest_15',display:'quest_15',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:200,
+		y:200
+		},
+		{name:'lesson_B',display:'lesson_B',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:300,
+		y:300
+		}
 	],
 	neighbours:[{display:'area A',
-		name:'areaA',//TODO: render data (x,y, icon,etc?)
+		name:'areaA',
+	},{display:'area C',
+		name:'areaC',
+	},{display:'area E',
+		name:'areaE',
+	}],
+};
+const areaC = {
+	name:'areaC',
+	towns:[
+		{name:'quest_14',display:'quest_14',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:0,
+		y:0
+		},
+		{name:'hotel',display:'hotel',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:100,
+		y:100
+		},
+		{name:'quest_13',display:'quest_13',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:200,
+		y:200
+		},
+		{name:'lesson_C',display:'lesson_C',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:250,
+		y:250
+		},
+		{name:'quest_12',display:'quest_12',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:250,
+		y:150
+		}
+	],
+	neighbours:[{display:'area B',
+		name:'areaB',
+	},{display:'area F',
+		name:'areaF',
+	}],
+};
+const areaD = {
+	name:'areaD',
+	towns:[
+		{name:'quest_11',display:'quest_11',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:0,
+		y:0
+		},
+		{name:'pub',display:'pub',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:100,
+		y:100
+		},
+		{name:'quest_10',display:'quest_10',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:200,
+		y:200
+		},
+		{name:'lesson_D',display:'lesson_D',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:250,
+		y:250
+		}
+	],
+	neighbours:[{display:'area A',
+		name:'areaA',
+	},{display:'area E',
+		name:'areaE',
+	},{display:'area G',
+		name:'areaG',
+	}],
+};
+const areaE = {
+	name:'areaE',
+	towns:[
+		{name:'quest_9',display:'quest_9',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:0,
+		y:0
+		},
+		{name:'hotel',display:'hotel',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:100,
+		y:100
+		},
+		{name:'quest_8',display:'quest_8',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:200,
+		y:200
+		},
+		{name:'lesson_E',display:'lesson_E',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:250,
+		y:250
+		}
+	],
+	neighbours:[{display:'area B',
+		name:'areaB',
+	},{display:'area F',
+		name:'areaF',
+	},{display:'area D',
+		name:'areaD',
+	},{display:'area B',
+		name:'areaB',
+	}],
+};
+const areaF = {
+	name:'areaF',
+	towns:[
+		{name:'quest_7',display:'quest_7',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:0,
+		y:0
+		},
+		{name:'hotel',display:'hotel',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:100,
+		y:100
+		},
+		{name:'pub',display:'pub',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:300,
+		y:300
+		},
+		{name:'quest_6',display:'quest_6',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:200,
+		y:200
+		},
+		{name:'lesson_F',display:'lesson_F',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:250,
+		y:250
+		}
+	],
+	neighbours:[{display:'area C',
+		name:'areaC',
+	},{display:'area E',
+		name:'areaE',
+	},{display:'area I',
+		name:'areaI',
+	}],
+};
+const areaG = {
+	name:'areaG',
+	towns:[
+		{name:'quest_5',display:'quest_5',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:0,
+		y:0
+		},
+		{name:'quest_4',display:'quest_4',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:200,
+		y:200
+		},
+		{name:'lesson_G',display:'lesson_G',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:250,
+		y:250
+		}
+	],
+	neighbours:[{display:'area D',
+		name:'areaD',
+	},{display:'area H',
+		name:'areaH',
+	}],
+};
+const areaH = {
+	name:'areaH',
+	towns:[
+		{name:'quest_3',display:'quest_3',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:0,
+		y:0
+		},
+		{name:'pub',display:'pub',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:100,
+		y:100
+		},
+		{name:'quest_2',display:'quest_2',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:200,
+		y:200
+		},
+		{name:'lesson_H',display:'lesson_H',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:250,
+		y:250
+		}
+	],
+	neighbours:[{display:'area E',
+		name:'areaE',
+	},{display:'area G',
+		name:'areaG',
+	},{display:'area I',
+		name:'areaI',
+	}],
+};
+const areaI = {
+	name:'areaI',
+	towns:[
+		{name:'quest_1',display:'quest_1',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:0,
+		y:0
+		},
+		{name:'hotel',display:'hotel',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:100,
+		y:100
+		},
+		{name:'hospital',display:'hospital',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:200,
+		y:200
+		},
+		{name:'lesson_I',display:'lesson_I',
+		starA:STAR_NONE,
+		starB:STAR_NONE,
+		starC:STAR_NONE,
+		x:250,
+		y:250
+		}
+	],
+	neighbours:[{display:'area F',
+		name:'areaF',
+	},{display:'area H',
+		name:'areaH',
 	}],
 };
 
 
+
+
 AreaData.areaA = areaA;
 AreaData.areaB = areaB;
+AreaData.areaC = areaC;
+AreaData.areaD = areaD;
+AreaData.areaE = areaE;
+AreaData.areaF = areaF;
+AreaData.areaG = areaG;
+AreaData.areaH = areaH;
+AreaData.areaI = areaI;
+
 
 export {AreaData,STAR_NONE};
--- src/data/ScriptData.js
+++ src/data/ScriptData.js
@@ -268,37 +268,221 @@ const quest_17 = [
 
 const quest_16 = [
 'label|start',
-'text|quest 16',
+'text|A large chest is buried in the ground',
+'text|Can you dig it up?',
+'text|skill check, 6 strength',
+'skill_check|{"amount":"6","skill":"STR","success":"script_success","fail":"script_fail"}',
+'pause|',
+'label|script_success',
+'text|you dug up the chest',
+"text|<unlocked quest: buried treasure>",
+`action|[
+	{"kind":"gain_keyword","value":"buried_treasure"}
+	]`,
+'text|inside is a simple pin with a yellow ribbon',
+'text|this was a symbol of road safety.',
+"text|you hold on to it, it's message is important.",
+'jump|done',
+'label|script_fail',
+'text|you could not dig up the chest',
+'label|done',
+'text|your party leaves the dig site',
 'done|'
 ];
 
 const quest_15 = [
 'label|start',
-'text|quest 15',
+'text|A misty hill conceals a small house',
+'text|Outside the house, a man works the yard',
+'pause|',
+'text|"hello!"',
+'text|the man is eager to greet you',
+'text|you approach the man',
+'pause|',
+"text|'I've been digging my yard for some time'",
+"text|'but I can't remember where it's gone'",
+"text|'... it must be this mist?'",
+'pause|',
+'text|the mist causes monsters to form',
+'text|I once had a symbol that could dispell the mist',
+'pause|',
+'if|{"keyword":"buried_treasure","label":"script_buried_treasure"}',
+'jump|done',
+'label|script_buried_treasure',
+'text|you show the man the pin you found',
+'text|"yes, remember that"',
+'text|the man if visibility relieved',
+'text|I can now show you how to get through fog.',
+'pause|',
+'text|drive using headlights.',
+'text|keep your distance from those in front.',
+'text|As a rule of thumb. keep at least 3 sconds away',
+'text|Keep your speed low',
+"text|<unlocked quest: dispell mist>",
+`action|[
+	{"kind":"gain_keyword","value":"dispell_mist"}
+	]`,
+'label|done',
+'text|you thank the man an leave',
 'done|'
 ];
 
 const quest_14 = [
 'label|start',
-'text|quest 14',
+'text|A stone tablet lies before you',
+'text|SEEK THE TENENTS',
+'pause|',
+'text|The simple text was reassuring',
+'text|It was advice about this realm.',
+'text|If you gain the tenents, you can return.',
+'pause|',
+'text|perhaps there was more to this tablet?',
+`choice|[
+	{"text":"Investigate","label":"script_choiceyes"},
+	{"text":"Leave","label":"script_choiceno"}
+	]`,
+'label|script_choiceyes',
+'text|Can you dig it up?',
+'text|skill check, 4 intelligence',
+'skill_check|{"amount":"4","skill":"INT","success":"script_success","fail":"script_fail"}',
+'label|script_success',
+'text|your party finds the stone is cold',
+'text|holding the tablet for a while warms it up',
+'text|slowly, new words appear',
+'pause|',
+'text|THERE ARE 8 TENENTS',
+'text|FIND THEM ALL',
+'text|AND RETURN TO YOUR REALM',
+"text|<unlocked quest: stone tablet>",
+`action|[
+	{"kind":"gain_keyword","value":"stone_tablet"}
+	]`,
+'pause|',
+'jump|script_choiceno',
+'label|script_fail',
+'text|You find nothing of interest.',
+'label|script_choiceno',
+'text|You leave the area.',
 'done|'
 ];
 
 const quest_13 = [
 'label|start',
-'text|quest 13',
+'text|At the side of the road,',
+'text|there is a small passageway.',
+'text|Witches Hats shield the path from traffic.',
+'pause|',
+'text|With fast-flowing traffic nearby,',
+'text|you need a bit of bravery to go inside',
+'pause|',
+'text|skill check, 5 bravery',
+'skill_check|{"amount":"5","skill":"BRV","success":"script_success","fail":"script_fail"}',
+'pause|',
+'label|script_success',
+'text|traffice spot you and slow down',
+'text|if everyone acted like this, ',
+"text|bravery wouldn't be needed at the roadside",
+'pause|',
+'text|Inside the passage is a set of spell runes',
+"text|<unlocked quest: spell rules>",
+`action|[
+	{"kind":"gain_keyword","value":"spell_rules"}
+	]`,
+'jump|done',
+'label|script_fail',
+"text|cars see you, but don't slow down",
+'text|if everyone was just a little more patient,',
+'text|you would be able to clear this passage easily',
+'pause|',
+"text|due to the insanity of greedy drivers",
+"text|your party suffers a debuff",
+"text|<-1 sanity to party>",
+`action|[
+	{"kind":"stat","stat":"sanity","value":-1,"character":"a"},
+	{"kind":"stat","stat":"sanity","value":-1,"character":"b"},
+	{"kind":"stat","stat":"sanity","value":-1,"character":"c"},
+	{"kind":"stat","stat":"sanity","value":-1,"character":"d"},
+	{"kind":"stat","stat":"sanity","value":-1,"character":"e"}
+	]`,
+
+'label|done',
+'text|You leave the area',
 'done|'
 ];
 
 const quest_12 = [
 'label|start',
-'text|quest 12',
+'text|You pull up to a forest lookout',
+'text|The scenery is nice and refreshing',
+'pause|',
+'text|something looks odd about the forest',
+'text|You can either try and figure it out',
+'text|by observing from the lookout',
+'text|or wandering through on foot',
+`choice|[
+	{"text":"Yes","label":"script_choiceabove"},
+	{"text":"No","label":"script_choicebelow"}
+	]`,
+'label|script_choiceabove',
+'text|skill check: 3 intelligence',
+'skill_check|{"amount":"3","skill":"INT","success":"script_success","fail":"script_fail"}',
+'label|script_success',
+'text|you figure out that the forest is cursed',
+'text|if you had entered on foot,',
+'text|it would have cost sanity.',
+'pause|',
+"text|<unlocked quest: forest>",
+`action|[
+	{"kind":"stat","stat":"sanity","value":-1,"character":"a"},
+	{"kind":"stat","stat":"sanity","value":-1,"character":"b"},
+	{"kind":"stat","stat":"sanity","value":-1,"character":"c"},
+	{"kind":"stat","stat":"sanity","value":-1,"character":"d"},
+	{"kind":"stat","stat":"sanity","value":-1,"character":"e"},
+	{"kind":"gain_keyword","value":"forest"}
+	]`,
+'jump|done',
+'label|script_fail',
+'text|you figure nothing out',
+'pause|',
+'jump|done',
+'label|script_choicebelow',
+'text|the forest is creepy.',
+'text|the forest is creepy..',
+'text|the forest is creepy...',
+'pause|',
+'text|the forest is creepy...',
+'text|the forest is creepy..',
+'text|the forest is creepy.',
+'pause|',
+'text|the forest is creepy.',
+"text|<unlocked quest: forest>",
+"text|<debuff: -1 sanity to party>",
+`action|[
+	{"kind":"stat","stat":"sanity","value":-1,"character":"a"},
+	{"kind":"stat","stat":"sanity","value":-1,"character":"b"},
+	{"kind":"stat","stat":"sanity","value":-1,"character":"c"},
+	{"kind":"stat","stat":"sanity","value":-1,"character":"d"},
+	{"kind":"stat","stat":"sanity","value":-1,"character":"e"},
+	{"kind":"gain_keyword","value":"forest"}
+	]`,
+'label|done',
+'text|You leave the forest.',
 'done|'
 ];
 
 const quest_11 = [
 'label|start',
-'text|quest 11',
+'text|a sign simply points ahead',
+'text|it shows a picture of a forest',
+'if|{"keyword":"forest","label":"script_forest"}',
+'text|what could it mean?',
+'jump|done',
+'pause|',
+'label|forest',
+'text|having visited the cursed forest',
+'text|your party works out the sign',
+'label|done',
+'text|You leave the area',
 'done|'
 ];
 
